f4029c0e22d42939cc17e1d4d709362379f2e306
Will Mernagh <wmernagh@gmail.com>
2014-05-27 06:56:37 -0400

    add doc.go

---
+++ lib/doc.go
@@ -0,0 +1,27 @@
+// Captain 'Jas' Hook: A Github Hook handler.
+//
+// Jas can be included in a net/http server to listen on a path for github webhooks.
+//
+// For a full guide visit http://github.com/wm/jas
+//
+// package main
+//
+// import (
+// 	"net/http"
+//
+// 	"github.com/wm/jas/lib"
+// )
+//
+// func main() {
+// 	j := jas.NewJas()
+// 	j.RegisterHandlerFunc(jas.PushPayloadLogger)
+// 	j.RegisterHandler(jas.NewFileChangeEmailer(jas.Options{
+// 		Emails: &[]string{"will@example.com", "eliot@example.com"},
+// 		Files:  &[]string{"db/structure.sql", "db/schema.rb"},
+// 	}))
+//
+// 	mux := http.NewServeMux()
+// 	mux.Handle("/", j)
+// 	http.ListenAndServe(":1234", mux)
+// }
+package jas
